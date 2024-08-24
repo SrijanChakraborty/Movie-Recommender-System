1a15848f76fcb5ba97ae1714751f34b92bba3a06
SrijanChakraborty <99581334+SrijanChakraborty@users.noreply.github.com>
2024-08-24 15:06:42 +0530

    commit

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
+
 from flask import Flask, render_template, request, jsonify
 import pickle
+import requests
 import pandas as pd
 
 app = Flask(__name__)
+
+
+def fetch_poster(movieId):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=ee46d7f2824234db2ff7c05c6c4f665d&language=en-US".format(
+        movieId)
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data["poster_path"]
+    if poster_path:
+        full_path = "https://image.tmdb.org/t/p/original/" + poster_path
+        return full_path
+    else:
+        return None
+    return full_path
+
+
 
 with open('D:/Git Folder/Movie-Recommender-System/PickleFile/movies_name.pkl', 'rb') as file:
     movie_dict = pickle.load(file)
@@ -12,7 +30,6 @@ movies = pd.DataFrame(movie_dict)
 @app.route('/')
 def home():
     movie_titles = movies['title'].tolist()
-
     return render_template('index.html', movie_titles=movie_titles)
 
 
